d97b8b92e6b227c619411e444eb6ee17e49d8250
dennistobar <dennis.tobar@gmail.com>
2018-06-18 16:15:28 -0400

    Algunos ajustes de estilo

--- miar.py
+++ miar.py
@@ -24,30 +24,20 @@ def preparar(url):
         #print('Este ISSN tiene página en MIAR, aquí: ' + url)
         return 1
 
-def cambiar(q):
+def cambiar(item):
     site = pywikibot.Site("wikidata", "wikidata")
     repo = site.data_repository()
-    item = pywikibot.ItemPage(repo, q)
     item.get()
     #Pongo el mismo formato que tendrá page cuando les comparo en el if
     nombre='[[wikipedia:es:' + str(item.sitelinks['eswiki']) + "]]"
     site2 = pywikibot.Site('es', 'wikipedia')
-    cat2 = pywikibot.Category(site2,'Categoría:Wikipedia:Artículos con plantilla MIAR')
-    gen = pg.CategorizedPageGenerator(cat2)
-    cont=0
-    for page in gen:
-        if (nombre==str(page)):
-            #Si entra aquí el artículo de wikidata que analizamos está en la categoría, no necesita {{miar}}
-            print(str(page) + ' ya tiene la etiqueta {{MIAR}}')
-            cont=1
-            break
-    if (cont==0):
-        #No está en la categoría, tenemos que añadir {{miar}}
-        #page = pywikibot.Page(site, str(item.sitelinks['eswiki']))
-        #page.text = u"{{MIAR}}"
-        #page.save(u"Aosbot añadiendo la etiqueta MIAR")
-        print('>>>>Se añadido la plantilla MIAR a ' + nombre)
-		
+    page_local = pywikibot.Page(source=site2, title=str(item.sitelinks['eswiki']))
+    if len(set(p.title(withNamespace=False)
+               for p in page_local.itertemplates()).intersection(['MIAR'])) != 0:
+        print ("{0} ya tiene la plantilla MIAR".format(page_local.title()))
+    else:
+        print ("{0} se debe añadir la plantilla MIAR".format(page_local.title()))
+
 def consulta(site=None):
     #Devuelve la consulta que necesitamos, los items con issn que tienen artículo en español
     query = """ SELECT ?item ?itemLabel ?ISSN WHERE {
@@ -61,26 +51,21 @@ def consulta(site=None):
     #print(query)
     return pg.WikidataSPARQLPageGenerator(query, site=site)
 
-def issn(repo,item):
+def issn(item):
     #print ('>>> La revista que está en ' + item + ' tiene este issn: ')
-    articulo = pywikibot.ItemPage(repo, item)
-    articulo.get()
+    item.get()
     sourcesid = 'P236'
-    if sourcesid in articulo.claims:
-        for source in articulo.claims[sourcesid]:
+    if sourcesid in item.claims:
+        for source in item.claims[sourcesid]:
             return(source.target)
-			
+
 if __name__ == "__main__":
     site = pywikibot.Site("wikidata", "wikidata")
     repo = site.data_repository()
     pages = consulta(site)
     for item in pages:
 	    #no sé como obtener el Q, por eso lo paso a string y lo corto hasta coger el Q entero
-        item=str(item)
-        item=item.split(":")
-        item=item[1].split("]")
-		#llamo a la función que me devuelve el issn del item
-        codigo=issn(repo,item[0])
+        codigo=issn(item)
         url = 'http://miar.ub.edu/issn/'+codigo
         if (preparar(url)):
-            cambiar(item[0])
+            cambiar(item)
