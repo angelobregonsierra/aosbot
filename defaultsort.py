0b3d82b497cb954bc0e6896d007963d8cbbe7b5b
dennistobar <dennis.tobar@gmail.com>
2018-10-10 11:41:36 -0300

    [feat] Rescribiendo defaultsort para que busque en categorías y otros

--- defaultsort.py
+++ defaultsort.py
@@ -1,32 +1,57 @@
-#Busca en una categoría dada todos los artículos para saber si tiene la plantilla defaultsort
-#Si la encuentra busca si tiene NF, en ese caso debe borrarse
+# Busca en una categoría dada todos los artículos para saber si tiene la plantilla defaultsort
+# Si la encuentra busca si tiene NF, en ese caso debe borrarse
 
 # -*- coding: utf-8 -*-
 from pywikibot import pagegenerators as pg
 import pywikibot
+import tools
 
-def main():
-    site = pywikibot.Site("es", "wikipedia")
-    wikidata_site = pywikibot.Site("wikidata", "wikidata")
-    categoria='Categoría:Personas'
-    cat = pywikibot.Category(site,categoria)
+site = pywikibot.Site("es", "wikipedia")
+
+
+def main(*args):
+
+    category = 'Personas'
+    local_args = pywikibot.handle_args(args)
+    for arg in local_args:
+        if arg.startswith('-category:'):
+            category = arg[10:]
+
+    cat = pywikibot.Category(site, 'Category:' + category)
     gen = pg.CategorizedPageGenerator(cat, recurse=True)
-    for page in gen:
-        print(page.title())
-        page_str = page.get()
-        tmpl_list = pywikibot.textlib.extract_templates_and_params(page_str)
-        for tipo in tmpl_list:
-            if ("DEFAULTSORT" in tipo) or ("Defaultsort" in tipo) or ("defaultsort" in tipo) or ("DefaultSort" in tipo):
-                try:
-                    item = pywikibot.ItemPage.fromPage(page)
-                    print("<<<<<<<<<<<<<<  " + page.title())
-                    f = open ("erroresDefaultSort.txt", mode='a', encoding='utf-8')
-                    f.write(str(page.title()) + '\n')
-                    f.close()
-                except:
-                    f = open ("erroresDefaultsort.txt", mode='a', encoding='utf-8')
-                    f.write("Exception: " + str(page.title()) + '\n')
-                    f.close()
+    articles = pg.PreloadingGenerator(gen)
+    for page in articles:
+        if page.isRedirectPage() or page.namespace() != 0:
+            continue
+        print('>>> {0}'.format(page.title()))
+        templates = pywikibot.textlib.extract_templates_and_params(page.get())
+        categories = page.categories()
+
+        isHuman = list(filter(lambda x: x.title(with_ns=False).lower() in [
+                       'mujeres', 'hombres'], categories))
+        hasNF = list(
+            filter(lambda x: x[0].lower() in ['nf', 'bd'], templates))
+        hasDefaultSort = list(
+            filter(lambda x: x[0].lower().find('defaultsort') > -1 or x[0].lower().find('ordenar') > -1, templates))
+        hasFicha = list(filter(lambda x: x[0].lower().find(
+            'ficha de') > -1, templates))
+
+        if (not len(hasFicha) or not len(isHuman)) and not len(hasNF):
+            """ Presumiblemente no es humano """
+            continue
+        if len(hasNF) and len(hasDefaultSort):
+            string = "[!!]\t{0}\ttiene DEFAULTSORT y NF".format(page.title())
+            tools.write(string, 'defaultsort.txt')
+        if len(hasDefaultSort) and not len(hasNF):
+            """ Tiene defaultsort, pero no NF """
+            string = "[?]\t{0}\ttiene solo DEFAULTSORT".format(page.title())
+            tools.write(string, 'defaultsort.txt')
+        if not len(hasDefaultSort) and not len(hasNF):
+            """ No tiene ni fu ni fa """
+            string = "[??]\t{0}\tno tiene clave de ordenamiento".format(
+                page.title())
+            tools.write(string, 'defaultsort.txt')
+
 
 if __name__ == "__main__":
     main()
